eeaa9d2f270d366686b74bcd9998bf9d89b2450b
Louis <sorrenti.l@northeastern.edu>
2022-11-24 21:06:28 -0500

    Completed assignment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
@@ -11,3 +12,7 @@ TuitsController(app)
 HelloController(app)
 UserController(app)
 app.listen(process.env.PORT || 4000);
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
